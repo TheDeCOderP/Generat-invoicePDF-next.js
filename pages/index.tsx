738173d9bd5c3b6581b9b6f34c4df6db445f6b4e
TheDeCOderP <113170140+TheDeCOderP@users.noreply.github.com>
2023-11-19 10:12:20 +0000

    PP

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import InvoicePage from './InvoicePage'
 function App() {
   return (
     <div className="app">
-      <h1 className="center fs-30">TaggedWeb Online Invoicing Software Tool - Create, Generate and Download Invoices or Invoice Templates as PDF</h1>
+      <h1 className="center fs-30">Generate PDF</h1>
       <InvoicePage />
     </div>
   )
